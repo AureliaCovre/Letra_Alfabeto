ea3cf5367051a588a18298ba60fd97d69f0204b6
AureliaCovre <aurelia-christina@hotmail.com>
2023-09-18 14:46:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+ # PosicaoNoAlfabeto
+""" Dada a letra N do alfabeto, nos diga qual a sua posição.
+Entrada: Um único caracter N, uma letra maiúscula ('A'-'Z') do alfabeto (que contém 26 letras).
+Saída: Um único inteiro, que representa a posição da letra no alfabeto.
+ 
+Entrada | Saída
+C 3
+J 10
+Z 26                         
+A 1 """
 # MÉTODO ord(): Retorna o valor  ASCII de cada letra ou símbolo do teclado;
 # TODO: De acordo com a entrada, imprima a posição dessa letra no Alfabeto;
 
